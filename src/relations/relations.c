b900887f7119a27c11047c93935bb73e584f9ca2
victoriavilasb <victoriaovilas+github@gmail.com>
2020-09-28 20:34:51 -0300

    fix: transictive test for i==i

--- src/relations/relations.c
+++ src/relations/relations.c
@@ -63,6 +63,10 @@ is_transictive(int size, int matrix[size][size])
     int i, j, w;
     for (i = 0; i < size; i++) {
         for (j = i; j < size; j++) {
+            if (matrix[i][j] && !matrix[i][i]) {
+                return 0;
+            }
+            
             if (matrix[i][j]) {
                 for (w = j; w < size; w++) {
                     if (matrix[j][w] && !matrix[i][w]) {
